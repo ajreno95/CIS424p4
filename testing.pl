60b46c7bbcf5877a3c9c813d7c877d1aa4717231
ajreno95 <A.RENO@vikes.csuohio.edu>
2017-11-16 14:07:33 -0500

    Update testing.pl

--- testing.pl
+++ testing.pl
@@ -53,16 +53,92 @@ mother(doris, cath).
 
 
 
+%Problem 1
+
+sibling(X,Y) :- 
+	father(F, X), 
+	father(F, Y), 
+	mother(M, X), 
+	mother(M, Y),
+	not(X = Y).
+ 
+sister(X,Y) :- 
+	sibling(X, Y), 
+	female(X). 
+
+uncle(X,Y) :-
+	father(F, Y),
+	mother(M, Y),
+	father(X, C),
+	mother(S, C),
+	(sibling(X, F); sibling(X, M); sibling(S, F); sibling(S, M)).
+
+grandson(X,Y) :- 
+	male(X), 
+	father(F,X), 
+	mother(M,X), 
+	(father(Y,F); mother(Y, F); father(Y, M); mother(Y, M)).
+
+descendant(X,Y) :- 
+	(father(Y,X); mother(Y,X)); 
+	(father(Y, F); mother(Y, F)), 
+	descendant(X, F). 
 
 
-sibling(X,Y) :- father(F, X), father(F, Y), male(F), mother(M, X), mother(M, Y), female(M).
-sister(X,Y) :- sibling(X, Y), female(X). 
-uncle(X,Y) :- father(F,Y), mother(M,Y), father(X, C), mother(S, C), (sibling(X, F); sibling(X, M); sibling(S, F); sibling(S, M)).
-grandson(X,Y) :- male(X), father(F,X), mother(M,X), (father(Y,F); mother(Y, F); father(Y, M); mother(Y, M)).
+%problem 2
 
+poly_add(A,[],A).
+poly_add([],B,B).
+poly_add([A|AT],[B|BT],[R|RT]) :-
+	R is A+B,
+	poly_add(AT, BT, RT).
 
+scalar_mult([],_,[]).
+scalar_mult([L|LT],S,[R|RT]):-
+	R is S*L,
+	scalar_mult(LT,S,RT).
 
+p_mult(_,[],[]).
+p_mult([],_,[]).
+p_mult(P,[Q|QT],R):-
+	p_mult([0.0|P], QT, R1),
+	scalar_mult(P, Q, R2),
+	poly_add(R2, R1, R).
 
+%Problem 3
 
+match(H, [H|T], T).
 
+nonterminalZ(Input, Result):-
+	(match(c, Input, NewResult), nonterminalZ(NewResult, Result));
+	match(c, Input, Result).
+nonterminalY(Input, Result):-
+	(match(b, Input, NewResult), nonterminalY(NewResult, Result));
+	match(b, Input, Result).
+nonterminalX(Input, Result):-
+	(match(a, Input, NewResult), nonterminalX(NewResult, Result));
+	match(a, Input, Result).
+s(Input, Result):-
+	nonterminalX(Input, FirstResult),
+	nonterminalY(FirstResult, SecondResult),
+	nonterminalZ(SecondResult, Result),!.
 
+%problem 4
+
+over35([clinton, obama, kerry, hclinton]).
+alive([clinton, obama, kerry, hclinton]).
+notFormerPresident([kerry, hclinton]).
+democrats([clinton, obama, kerry, hclinton]).
+
+tests(Candidate):-
+	(notFormerPresident(NFPList), member(Candidate, NFPList)),
+	(alive(AliveList), member(Candidate, AliveList)),
+	(over35(O35List), member(Candidate, O35List)).
+	
+member(Element, [Element | _]):-!.
+member(Element, [_ | List]):-
+	member(Element, List).
+	
+dem_candidate(Candidate):-
+	democrats(DemList), member(Candidate, DemList), 
+	tests(Candidate).
